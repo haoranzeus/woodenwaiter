96b36bd3eaeb5db13130c13276aeadea00aacd06
zhanghaoran <haoranzeus@163.com>
2017-07-20 17:28:21 +0800

    [新增] 新增setup.py的一些信息，版本号改为0.0.2

--- setup.py
+++ setup.py
@@ -4,9 +4,10 @@ from setuptools import setup, find_packages
 
 setup(
     name='woodenwaiter',
-    version='0.0.1',
+    version='0.0.2',
     author='zhanghaoran',
     author_email='haoranzeus@gmail.com',
+    url='https://github.com/haoranzeus/woodenwaiter',
     description='A producer-customer model based on redis',
     packages=find_packages(exclude=['tests']),
     install_requires=['redis'],
