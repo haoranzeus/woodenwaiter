55eb93245bf02c634db41325a5d11c37c208538d
zhanghaoran <haoranzeus@163.com>
2017-07-20 16:55:53 +0800

    [修改].gitignore忽略svn。setup.py完善内容

--- setup.py
+++ setup.py
@@ -5,6 +5,9 @@ from setuptools import setup, find_packages
 setup(
     name='woodenwaiter',
     version='0.0.1',
+    author='zhanghaoran',
+    author_email='zhanghaoran@cmhi.chinamobile.com',
+    description='A producer-customer model based on redis',
     packages=find_packages(exclude=['tests']),
     install_requires=['redis'],
 )
