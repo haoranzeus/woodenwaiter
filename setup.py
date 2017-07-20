589a38d53a4225e4a052044354d483eb99fa8638
zhanghaoran <haoranzeus@163.com>
2017-07-20 17:30:12 +0800

    [新增] setup.py添加长描述

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python3
 from setuptools import setup, find_packages
 
+readme = open('README.rst').read()
 
 setup(
     name='woodenwaiter',
-    version='0.0.1',
+    version='0.0.3',
     author='zhanghaoran',
     author_email='zhanghaoran@cmhi.chinamobile.com',
     description='A producer-customer model based on redis',
+    long_description=readme,
     packages=find_packages(exclude=['tests']),
     install_requires=['redis'],
 )
